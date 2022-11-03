32538b4fd1585c6a4c4146ea18195db6cac9b555
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2022-11-03 18:53:57 +0545

    frontend completed

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -30,14 +30,14 @@ const ProfileCard = () => {
 
       <div className="mt-3 ">
         <label
-          className="input_label"
+          className="input_label px-2"
           htmlFor="address">
           Wallet Address
         </label>
 
-        <div className="input_field d-flex align-items-center mt-1 ">
+        <div className="input_field d-flex align-items-center mt-1 px-2 ">
           <FaUserAlt
-            color="#fe00fe"
+            color="#1e5262"
             size={20}
             cursor="pointer"
           />
@@ -50,44 +50,50 @@ const ProfileCard = () => {
         </div>
       </div>
 
-      <div className="mt-4">
+      <div className="mt-4 ">
         <label
-          className="input_label"
+          className="input_label px-2"
           htmlFor="amount">
           Wallet Amount (ETH)
         </label>
 
-        <div className="input_field d-flex align-items-center mt-1 ">
+        <div className="input_field d-flex align-items-center mt-1 px-1">
           <SiEthereum
-            color="#fe00fe"
-            size={40}
+            color="#1e5262"
+            size={30}
             cursor="pointer"
           />
 
-          <input
-            readOnly
-            className="input_field border-0 rounded-0 py-2 px-2  "
-            placeholder="0"
-          />
-
-          <BeatLoader
-            color="#1e5262"
-            className="input_field border-0 rounded-0 py-2 mt-1 "
-          />
+          <div className="d-flex">
+            <BeatLoader
+              color="#1e5262"
+              className="input_field border-0 rounded-0 py-2 mt-1 "
+            />
+          </div>
 
           <input
             readOnly
             className="input_field border-0 rounded-0 py-2 px-2  "
             placeholder="0"
           />
+
+          
+
         </div>
       </div>
 
-      <h4 className="text-center">Connect with Your Metamask</h4>
-      <p className="text-center ">
+      <h4 className="text-center mt-4">Connect with Your Metamask</h4>
+      <p className="text-center  ">
         You must have to connect your Metamask <br /> wallet to add the task .
       </p>
-      <button className="custom_button  my-2">Connect Wallet</button>
+
+      <div className="mt-4 px-2">
+        <button
+          type="button"
+          className=" w-100 card_connect_button d-flex align-items-center justify-content-center btn px-4   rounded-pill">
+          Connect Wallet
+        </button>
+      </div>
     </div>
   );
 };
--- frontend/src/components/TasksCard.tsx
+++ frontend/src/components/TasksCard.tsx
@@ -1,47 +1,31 @@
-import React, { useState } from "react";
-import { RiSendPlaneFill } from "react-icons/ri";
-import { ethers } from "ethers";
+import React from "react";
 
 const TasksCard = () => {
   return (
-    // <div className="card task_card">
-    //   <div className="card-body">
-    //
-
-    //
-
-    //     <div className="d-flex align-items-center  justify-content-between">
-    //       <div className="d-flex gap-3  ">
-    //         <h5>#1</h5>
-    //         <h5>ddd</h5>
-    //       </div>
-
-    //
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="task_card">
       <div className="input_field  d-flex align-items-center  justify-content-between py-3  px-2">
         <input
           className=" input_field border-0   "
           placeholder="Add You task ..."
         />
-        <button
-          type="button"
-          className="custom_button  rounded-pill py-2 px-5">
-          Add
-        </button>
+        <div className=" px-4 d-flex container justify-content-end">
+          <button
+            type="button"
+            className=" custom_button d-flex align-items-center justify-content-center btn px-4 py-2 rounded-pill">
+            Add Task
+          </button>
+        </div>
       </div>
 
-      <h2 className="text-center mb-4">Your Recent Tasks</h2>
+      <h2 className="text-center my-5">Your Recent Tasks</h2>
 
-      <table className="table table-hover ">
+      <table className="table text_color ">
         <thead>
           <tr>
             <th scope="col">#</th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
+            <th scope="col">Handle</th>
             <th scope="col">Handle</th>
           </tr>
         </thead>
@@ -50,37 +34,21 @@ const TasksCard = () => {
             <th scope="row">1</th>
             <td>Mark</td>
             <td>Otto</td>
-            <td className="pt-3 px-4 d-flex container justify-content-end">
-              <button
-                type="button"
-                className="custom_button d-flex align-items-center justify-content-center btn px-2 py-1 rounded-pill">
-                Complete
-              </button>
-
-              <button
-                type="button"
-                className="custom_button d-flex align-items-center justify-content-center btn px-2 py-1 rounded-pill">
-                Delete
-              </button>
-            </td>
-          </tr>
-
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
             <td>Otto</td>
-            <td className="pt-3 px-4 d-flex container justify-content-end">
-              <button
-                type="button"
-                className="custom_button d-flex align-items-center justify-content-center btn px-2 py-1 rounded-pill">
-                Complete
-              </button>
+            <td>
+              <div className=" d-flex gap-2 ">
+                <button
+                  type="button"
+                  className="custom_button d-flex align-items-center justify-content-center btn px-2 py-1 rounded-pill">
+                  Complete
+                </button>
 
-              <button
-                type="button"
-                className="custom_button d-flex align-items-center justify-content-center btn px-2 py-1 rounded-pill">
-                Delete
-              </button>
+                <button
+                  type="button"
+                  className="custom_button d-flex align-items-center justify-content-center btn px-3 py-1 rounded-pill">
+                  Delete
+                </button>
+              </div>
             </td>
           </tr>
         </tbody>
